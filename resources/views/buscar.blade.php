39ec26be8881d942b623f79dd7ed40567d90292f
German E Loaisiga <121838586+loaisigager@users.noreply.github.com>
2024-07-06 10:59:18 -0600

    cambio de metodo a count

--- resources/views/buscar.blade.php
+++ resources/views/buscar.blade.php
@@ -117,7 +117,7 @@
       <div id="item2"> <!-- Segundo Child Flex Container -->
         <div class="scrollable-content">
         <h2>Resultados</h2>
-        <p>Se encontraron {{ $inmuebles->total() }} resultados.</p> <!-- $inmuebles es la clase creada en el modelo inmueble.php aca se llama al metodo total de las librerias de Laravel,esto muestra el total de los resultados encontrados-->
+        <p>Se encontraron {{ $inmuebles->count() }} resultados.</p> <!-- $inmuebles es la clase creada en el modelo inmueble.php aca se llama al metodo total de las librerias de Laravel,esto muestra el total de los resultados encontrados-->
         @forelse ($inmuebles as $inmueble)
           <div id="informacion">
           <!-- Muestra la información del inmueble aquí es igual que la descripcion anterior de la clase en el modelo-->
